552bceae23910aeda3bfc9193e31e5763ce1bef0
Yasith Eranda Yapa Amarasekara <106188847+yashEra@users.noreply.github.com>
2023-12-14 13:48:15 +0530

    Project Started

---
+++ app/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dice_roller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
